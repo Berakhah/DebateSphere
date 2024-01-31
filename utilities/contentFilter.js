31ad7f48a475ea054d6ece73a39dc871e1f47aea
Berakhah <97037825+Berakhah@users.noreply.github.com>
2024-01-31 12:30:28 +0300

    Folder Restructure

--- utilities/contentFilter.js
+++
@@ -1,6 +0,0 @@
-// utilities/contentFilter.js
-const prohibitedPatterns = [/inappropriate/i, /bannedword/i];
-
-const isContentAppropriate = (text) => !prohibitedPatterns.some((pattern) => pattern.test(text));
-
-module.exports = { isContentAppropriate };
